640156a1f6e83b47ae3b88884adb1a47462a47ff
7beastgamer <sameerasimhajayasimhabsc24@rvu.edu.in>
2026-02-20 09:40:58 +0530

    Add two sum solution

---
+++ solution.py
@@ -0,0 +1,10 @@
+echo 'def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], i]
+        seen[num] = i
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))' > solution.py
